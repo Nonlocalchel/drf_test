f0f0a18aba90a6c6aebed201c1cb11319ba768a9
Slava <v4slava.guk@gmail.com>
2024-10-16 21:55:43 +0300

    docker settings good

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 FROM python:3.12.2
 
+ENV PYTHONDONTWRITEBITECODE 1 #запрещаем python буферизироваться
+ENV PYTHONUNBUFFERED 1
+
 COPY requirements.txt /temp/requirements.txt
 RUN pip install --upgrade pip
 RUN pip install -r /temp/requirements.txt
 
 COPY backend /backend
 WORKDIR /backend
+COPY .env /.env
 
 EXPOSE 8000
 
--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -2,24 +2,24 @@ from .base import *
 from .simple_jwt_config import SIMPLE_JWT
 from .swagger_config import SWAGGER_SETTINGS
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': 'localhost',
-#         'PORT': 5432,
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
+        'HOST': 'database',
         'PORT': 5432,
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('POSTGRES_DB'),
+#         'USER': os.environ.get('POSTGRES_USER'),
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+#         'HOST': 'database',#database #127.0.0.1
+#         'PORT': 5432,
+#     }
+# }
