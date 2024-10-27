06106fc076058e9f26e600ec6f651a6202f667e9
Slava <v4slava.guk@gmail.com>
2024-10-27 11:03:50 +0300

    refactor auth custom class

--- backend/users/authenticate.py
+++ backend/users/authenticate.py
@@ -1,36 +1,16 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication, AuthUser
-from django.utils.translation import gettext_lazy as _
-from rest_framework_simplejwt.exceptions import AuthenticationFailed, InvalidToken
-from rest_framework_simplejwt.settings import api_settings
-from rest_framework_simplejwt.tokens import Token
-from rest_framework_simplejwt.utils import get_md5_hash_password
+from services.mixins.JWTAuthenticationWithCustomUserGet import JWTAuthenticationWithCustomUserGet, AuthUser, \
+    api_settings
+
+from users.utils import get_all_user_types, get_auth_users_fields
 
 
-class Authenticate(JWTAuthentication):
-    def get_user(self, validated_token: Token) -> AuthUser:
-        """
-        Attempts to find and return a user using the given validated token.
-        """
-        try:
-            user_id = validated_token[api_settings.USER_ID_CLAIM]
-        except KeyError:
-            raise InvalidToken(_("Token contained no recognizable user identification"))
+class Authenticate(JWTAuthenticationWithCustomUserGet):
 
-        try:
-            # user = self.user_model.objects.get(**{api_settings.USER_ID_FIELD: user_id})
-            user = self.user_model.objects.select_related('worker').get(id=user_id)
-        except self.user_model.DoesNotExist:
-            raise AuthenticationFailed(_("User not found"), code="user_not_found")
+    def get_user_orm_fetch(self, user_id) -> AuthUser:
+        types = get_all_user_types()
+        fields = get_auth_users_fields(types)
 
-        if not user.is_active:
-            raise AuthenticationFailed(_("User is inactive"), code="user_inactive")
-
-        if api_settings.CHECK_REVOKE_TOKEN:
-            if validated_token.get(
-                    api_settings.REVOKE_TOKEN_CLAIM
-            ) != get_md5_hash_password(user.password):
-                raise AuthenticationFailed(
-                    _("The user's password has been changed."), code="password_changed"
-                )
-
+        user_manager = self.user_model.objects
+        user_fetch = user_manager.select_related(*types).only(*fields)
+        user = user_fetch.get(**{api_settings.USER_ID_FIELD: user_id})
         return user
--- backend/users/utils.py
+++ backend/users/utils.py
@@ -6,3 +6,13 @@ def figure_deleted_data(user_role: str | None) -> str:
         return User.UserType.CUSTOMER
 
     return User.UserType.WORKER
+
+
+def get_all_user_types() -> list[str]:
+    return [user_type.value for user_type in User.UserType]
+
+
+def get_auth_users_fields(related_names) -> list[str]:
+    basic_user_filds = [field.name for field in User._meta.fields]
+    related_fields = [related_name+'__id' for related_name in related_names]
+    return basic_user_filds + related_fields
