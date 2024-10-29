431731414778d86c992efab528e2bd76f4b3740a
Slava <v4slava.guk@gmail.com>
2024-10-30 00:51:02 +0300

    add e letter

--- backend/users/utils/auth_utils.py
+++ backend/users/utils/auth_utils.py
@@ -3,9 +3,9 @@ from users.models import User
 
 def get_auth_users_fields(related_names) -> list[str]:
     """Construct needed fields in auth request for optimization"""
-    basic_user_filds = [field.name for field in User.get_model_fields()]
+    basic_user_fields = [field.name for field in User.get_model_fields()]
     extended_fields = [related_name + '__id' for related_name in related_names]
-    return basic_user_filds + extended_fields
+    return basic_user_fields + extended_fields
 
 
 def get_user_types() -> list:
