248936eaf22413a9113b62198f246828c82e4ae6
Slava <v4slava.guk@gmail.com>
2024-11-05 11:45:59 +0300

    clean docker compose and add postgre settings to settings.dev.py

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -4,14 +4,14 @@ from .swagger_config import SWAGGER_SETTINGS
 
 LOGGING = {}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': '',
-#         'PORT': 5432,
-#         'CONN_MAX_AGE': 60
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': '',
+        'PORT': 5432,
+        'CONN_MAX_AGE': 60
+    }
+}
